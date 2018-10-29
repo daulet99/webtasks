ee0acadc2309d6bf68dacad1c536a107c5878f27
Daulet Sergazin <33065516+daulet99@users.noreply.github.com>
2018-10-29 08:12:46 +0600

    Add files via upload

---
+++ week7-8/script2.js
@@ -0,0 +1,56 @@
+function err() {
+	document.getElementById("loading").style.display = "none";
+	const btn = document.querySelectorAll('button');
+	btn[0].textContent = "Some Error Occured";
+}
+function onResponse(response) {
+	if(response.ok) {
+		return response.json();
+	}
+	else {
+		err()
+		return ;
+	}
+}
+class Car {
+	constructor(carContainer, maker, model, price) {
+		const card = document.createElement('div');
+		const ma = document.createElement('span');
+		const mo = document.createElement('span');
+		const pr = document.createElement('span');
+		ma.innerHTML = maker;
+		mo.innerHTML = model;
+		pr.innerHTML = price;
+		card.className = "card";
+		ma.className = "title";
+		mo.className = "title";
+		pr.className = "price";
+		card.appendChild(ma)
+		card.appendChild(mo)
+		card.appendChild(pr);
+		carContainer.appendChild(card);
+	}
+}
+function onJsonReady(data) {
+	this.carInfo = data;
+	this.printCars();
+}
+function printCars() {
+	const carContainer = document.querySelector('#cards');
+	carContainer.innerHTML = '';
+	for(const info of this.carInfo) {
+		const car = new Car(carContainer, info.maker, info.model, info.price);
+	}
+	const btn = document.querySelectorAll('button');
+	btn[0].textContent = "Items are loaded";
+}
+function load() {
+	document.getElementById("loading").style.display = "flex";
+	const btn = document.querySelectorAll('button');
+	btn[0].textContent = "Loading";
+	fetch("http://demo4296963.mockable.io/listCars").catch(err).then(onResponse).then(onJsonReady);
+}
+const btn = document.querySelectorAll('button');
+for (const i of btn) {
+    i.addEventListener('click', load);
+}
